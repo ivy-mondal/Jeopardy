a1e2fb482cbe66957c7a05387d579348e80fc5cb
ivy mondal <ivy94635@gmail.com>
2024-11-05 15:43:00 +0530

    now data is dataing

--- prepare_data.py
+++ prepare_data.py
@@ -1,10 +1,12 @@
 # Gotta prepare Questions and answers
 import json
+import re
 
 with open('JEOPARDY_QUESTIONS1.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-extract_data = [{"question": item["question"], "answer": item["answer"]} for item in data]
+extract_data = [{"topic": item["category"],"question": item["question"], "answer": item["answer"], "points": item["value"]} for item in data
+                if not ('<a href' in item["question"] or '.mp3' in item["question"])]
 
 with open('dataset.json', 'w') as f:
     json.dump(extract_data, f, indent=4)
