8bc58942e7392c06e379824d4dcf7a5f28e6bf79
ivy mondal <ivy94635@gmail.com>
2024-11-07 11:28:37 +0530

    questions done UwU

--- prepare_data.py
+++ prepare_data.py
@@ -1,15 +1,17 @@
 # Gotta prepare Questions and answers
 import json
-import re
 
 with open('JEOPARDY_QUESTIONS1.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
-extract_data = [{"topic": item["category"],"question": item["question"], "answer": item["answer"], "points": item["value"]} for item in data
-                if not ('<a href' in item["question"] or '.mp3' in item["question"] or '<br' in item["question"])]
+extract_data = [
+    {"topic": item["category"],
+     "question": item["question"],
+     "answer": item["answer"],
+     "value": int(item["value"].strip("$").replace(",", "")) if item["value"] is not None else 0}
+    for item in data
+    if not ('<a href' in item["question"] or '.mp3' in item["question"] or '<br' in item["question"])
+]
 
 with open('dataset.json', 'w') as f:
     json.dump(extract_data, f, indent=4)
-
-unique_points = {int(item["value"]) for item in data}
-print(sorted(unique_points))
---
+++ sampling_questions.py
@@ -0,0 +1,30 @@
+# making questions for rounds
+import json
+from collections import Counter
+
+with open('dataset.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+total_items = len(data)
+print(f"There are {total_items} questions in this trivia treasure trove!")
+unique_points = {item["value"] for item in data}
+print(sorted(unique_points))
+point_counts = Counter(item["value"] for item in data if item["value"] is not None)
+
+for value, count in point_counts.most_common():
+    print(f"{value:<5} shows up {count:,} times")  # The :<5 aligns the values nicely
+
+
+def create_question_level(data, min_value, max_value, file_name):
+    questions_and_answers = [
+        {"question": item["question"], "answer": item["answer"]} for item in data if min_value < item["value"] <= max_value and item["value"] % 5 == 0
+    ]
+    with open(file_name, 'w') as f:
+        json.dump(questions_and_answers, f, indent=4)
+
+
+create_question_level(data, 0, 300, "level_1_questions")
+create_question_level(data, 300, 600, "level_2_questions")
+create_question_level(data, 600, 1000, "level_3_questions")
+create_question_level(data, 1000, 2000, "level_4_questions")
+create_question_level(data, 2000, 18000, "level_5_questions")
