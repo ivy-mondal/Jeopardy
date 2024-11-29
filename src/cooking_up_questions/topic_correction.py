d81d9cfc6ec3f3e8bc901efbddd903befc6ace83
ivy mondal <ivy94635@gmail.com>
2024-11-29 09:30:28 +0530

    I forgor

--- src/cooking_up_questions/topic_correction.py
+++ src/cooking_up_questions/topic_correction.py
@@ -3,7 +3,9 @@ import json
 with open('../../datasets/topics.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
+data = [item.replace("\u201c", '"') for item in data]
+data = [item.replace("\u201d", '"') for item in data]
+data = [item.replace("\u00d1", "Ñ") for item in data]
 data = [item.replace('\"', '') for item in data]
-
 with open('../../datasets/topics.json', 'w', encoding='utf-8') as file:
     json.dump(data, file)
--- src/cooking_up_questions/topics_list_maker.py
+++ src/cooking_up_questions/topics_list_maker.py
@@ -1,16 +1,18 @@
 import json
 
 
-def create_topic_list(data):
+def create_topic(data):
     topic_list = []
     for item in data:
         if item["topic"] not in topic_list:
             topic_list.append(item["topic"])
+
     with open('../../datasets/topics.json', 'w') as f:
         json.dump(topic_list, f, indent=4)
+    return topic_list
 
 
 with open('../../datasets/dataset.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-create_topic_list(data)
 
+topics = create_topic(data)
